ba09db864a4564ef63ceb44c1bc867da23a04613
Alec Fekete <alec.fekete@gmail.com>
2016-01-23 22:43:02 -0600

    Uses cordova background geolocation plugin
    to center map

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -76,6 +76,73 @@ angular.module('starter.controllers', [])
   $scope.mapCreated = function(map) {
     $scope.map = map;
 
+    // Get a reference to the plugin.
+    var bgGeo = window.BackgroundGeolocation;
+
+    /**
+    * This callback will be executed every time a geolocation is recorded in the background.
+    */
+    var callbackFn = function(location, taskId) {
+        var coords = location.coords;
+        var lat    = coords.latitude;
+        var lng    = coords.longitude;
+
+        $scope.map.setCenter(new google.maps.LatLng(lat, lng));
+
+        // Simulate doing some extra work with a bogus setTimeout.  This could perhaps be an Ajax request to your server.
+        // The point here is that you must execute bgGeo.finish after all asynchronous operations within the callback are complete.
+        setTimeout(function() {
+          bgGeo.finish(taskId); // <-- execute #finish when your work in callbackFn is complete
+        }, 1000);
+    };
+
+    var failureFn = function(error) {
+        console.log('BackgroundGeoLocation error');
+    }
+
+    // BackgroundGeoLocation is highly configurable.
+    bgGeo.configure(callbackFn, failureFn, {
+        // Geolocation config
+        desiredAccuracy: 0,
+        stationaryRadius: 50,
+        distanceFilter: 50,
+        disableElasticity: false, // <-- [iOS] Default is 'false'.  Set true to disable speed-based distanceFilter elasticity
+        locationUpdateInterval: 5000,
+        minimumActivityRecognitionConfidence: 80,   // 0-100%.  Minimum activity-confidence for a state-change 
+        fastestLocationUpdateInterval: 5000,
+        activityRecognitionInterval: 10000,
+        stopDetectionDelay: 1,  // Wait x minutes to engage stop-detection system
+        stopTimeout: 2,  // Wait x miutes to turn off location system after stop-detection
+        activityType: 'AutomotiveNavigation',
+
+        // Application config
+        debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
+        forceReloadOnLocationChange: false,  // <-- [Android] If the user closes the app **while location-tracking is started** , reboot app when a new location is recorded (WARNING: possibly distruptive to user) 
+        forceReloadOnMotionChange: false,    // <-- [Android] If the user closes the app **while location-tracking is started** , reboot app when device changes stationary-state (stationary->moving or vice-versa) --WARNING: possibly distruptive to user) 
+        forceReloadOnGeofence: false,        // <-- [Android] If the user closes the app **while location-tracking is started** , reboot app when a geofence crossing occurs --WARNING: possibly distruptive to user) 
+        stopOnTerminate: false,              // <-- [Android] Allow the background-service to run headless when user closes the app.
+        startOnBoot: true,                   // <-- [Android] Auto start background-service in headless mode when device is powered-up.
+
+        // HTTP / SQLite config
+        url: 'http://posttestserver.com/post.php?dir=cordova-background-geolocation',
+        method: 'POST',
+        batchSync: true,       // <-- [Default: false] Set true to sync locations to server in a single HTTP request.
+        autoSync: true,         // <-- [Default: true] Set true to sync each location to server as it arrives.
+        maxDaysToPersist: 1,    // <-- Maximum days to persist a location in plugin's SQLite database when HTTP fails
+        headers: {
+            "X-FOO": "bar"
+        },
+        params: {
+            "auth_token": "maybe_your_server_authenticates_via_token_YES?"
+        }
+    });
+
+    // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
+    bgGeo.start();
+
+    // If you wish to turn OFF background-tracking, call the #stop method.
+    // bgGeo.stop()
+
     $scope.infoWindow = new google.maps.InfoWindow();
 
     $scope.infoWindow.addListener('closeclick', function(){
@@ -186,25 +253,25 @@ angular.module('starter.controllers', [])
     });
   };
 
-  $scope.centerOnMe = function () {
+  // $scope.centerOnMe = function () {
     
-    if (!$scope.map) {
-      return;
-    }
+  //   if (!$scope.map) {
+  //     return;
+  //   }
 
-    $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
+  //   $scope.loading = $ionicLoading.show({
+  //     content: 'Getting current location...',
+  //     showBackdrop: false
+  //   });
 
-    navigator.geolocation.getCurrentPosition(function (pos) {
+  //   navigator.geolocation.getCurrentPosition(function (pos) {
       
-      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      $scope.loading.hide();
-    }, function (error) {
-      alert('Unable to get location: ' + error.message);
-    });
-  };
+  //     $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+  //     $scope.loading.hide();
+  //   }, function (error) {
+  //     alert('Unable to get location: ' + error.message);
+  //   });
+  // };
 
   $scope.toggleMarkers = function() {
     for (var i=0; i < $scope.markers.length; i++){
