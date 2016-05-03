4b017d75674e67bca2ab486a29ee8ba2a10d5ff3
Matt Yoder <myoder@ccrpc.org>
2016-05-03 11:59:25 -0500

    Removed unused distance code.

--- www/js/services.js
+++ www/js/services.js
@@ -107,23 +107,6 @@ angular.module('starter.services', [])
     }
     var service = {};
 
-    // http://www.movable-type.co.uk/scripts/latlong.html
-    /*var distance = function(lat1, lng1, lat2, lng2) {
-      var R = 6371000; // metres
-      var φ1 = lat1.toRadians();
-      var φ2 = lat2.toRadians();
-      var Δφ = (lat2-lat1).toRadians();
-      var Δλ = (lon2-lon1).toRadians();
-
-      var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-              Math.cos(φ1) * Math.cos(φ2) *
-              Math.sin(Δλ/2) * Math.sin(Δλ/2);
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-      var d = R * c;
-      return d;
-    }*/
-
     // http://stackoverflow.com/a/21623256
     function distance(lat1, lon1, lat2, lon2) {
       var R = 6371; // Radius of the earth in km
