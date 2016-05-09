e471739a61a2897037a9cbd37d824d103efbc7bc
Matt Yoder <myoder@ccrpc.org>
2016-05-09 10:14:28 -0500

    Updated settings to better match previous versions.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -37,12 +37,18 @@ angular.module('starter.controllers', [])
       STATUS_RECORDING = 'recording',
       STATUS_PAUSED = 'paused',
       BG_PLUGIN_SETTINGS = {
-          debug: false,
-          desiredAccuracy: 10, // 10 meters
-          distanceFilter: 50, // Generate update events every 50 meters
-          disableElasticity: true, // Do not auto-adjust distanceFilter
           activityType: 'Fitness', // iOS activity type
-          stopTimeout: 99999 // Keep tracking for 3 minutes while stationary
+          autoSync: false, // Do not automatically post to the server
+          debug: false, // Disable debug notifications
+          desiredAccuracy: 0, // Highest accuracy
+          distanceFilter: 20, // Generate update events every 20 meters
+          disableElasticity: true, // Do not auto-adjust distanceFilter
+          fastestLocationUpdateInterval: 1000, // Prevent updates more than once per second (Android)
+          locationUpdateInterval: 5000, // Request updates every 5 seconds (Android)
+          startOnBoot: false, // Do not start tracking on device boot
+          stationaryRadius: 20, // Activate the GPS after 20 meters (iOS)
+          stopOnTerminate: true, // Stop geolocation tracking on app exit
+          stopTimeout: 3 // Keep tracking for 3 minutes while stationary
         },
       bgGeo,
       currentLocation,
